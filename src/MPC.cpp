9e97b9632c05835039539c705c3fd2d698c3aa24
Steven Lee <steve.lee182@gmail.com>
2018-02-15 13:09:19 -0500

    add penalty for speed and steer combined particulary to handle the first turn

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,8 +50,8 @@ class FG_eval {
     fg[0] = 0;
 
     for (int t = 0; t < N; t++) {
-      fg[0] += 2000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
-      fg[0] += 2000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
+      fg[0] += 3000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
+      fg[0] += 3000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v,2);
     }
 
@@ -59,6 +59,7 @@ class FG_eval {
     for (int t = 0; t < N - 1; t++) {
       fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations
