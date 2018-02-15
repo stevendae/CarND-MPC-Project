fa07de24db704866a99e47621fa6afd7c5235062
Steven Lee <steve.lee182@gmail.com>
2018-02-15 18:28:28 -0500

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 10;
-double dt = 0.1;
+size_t N = 15;
+double dt = 0.05;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -221,7 +221,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   options += "Sparse  true        reverse\n";
   // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
   // Change this as you see fit.
-  options += "Numeric max_cpu_time 1.5\n";
+  options += "Numeric max_cpu_time 0.5\n";
 
   // place to return solution
   CppAD::ipopt::solve_result<Dvector> solution;
