559795c8e0057f34108f1c14b7538b7621c6fb36
Steven Lee <steve.lee182@gmail.com>
2018-02-15 18:42:37 -0500

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -57,14 +57,14 @@ class FG_eval {
 
     // Minimize the use of actuators
     for (int t = 0; t < N - 1; t++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 1000*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 100*CppAD::pow(vars[a_start + t], 2);
       //fg[0] += 750*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2); // minimize speed while turning
     }
 
     // Minimize the value gap between sequential actuations
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 500 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t],2);
+      fg[0] += 1000 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t],2);
       fg[0] += 100 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
