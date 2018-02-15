137a59a59e20c635e77712b31aabc700bd60ca67
Steven Lee <steve.lee182@gmail.com>
2018-02-15 17:14:36 -0500

    Fine Tuning Cost Function

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,16 +50,16 @@ class FG_eval {
     fg[0] = 0;
 
     for (int t = 0; t < N; t++) {
-      fg[0] += 3000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
-      fg[0] += 3000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
-      fg[0] += CppAD::pow(vars[v_start + t] - ref_v,2);
+      fg[0] += 5000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
+      fg[0] += 5000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
+      fg[0] += 10*CppAD::pow(vars[v_start + t] - ref_v,2);
     }
 
     // Minimize the use of actuators
     for (int t = 0; t < N - 1; t++) {
       fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
-      fg[0] += 500*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2);
+      fg[0] += 8000*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2); // minimize speed while turning
     }
 
     // Minimize the value gap between sequential actuations
