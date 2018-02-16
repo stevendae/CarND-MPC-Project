b4d6199ad1f97e5a790aa023c1337b2b9a3f6a4c
Steven Lee <steve.lee182@gmail.com>
2018-02-16 14:09:05 -0500

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,22 +50,22 @@ class FG_eval {
     fg[0] = 0;
 
     for (int t = 0; t < N; t++) {
-      fg[0] += 2000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
-      fg[0] += 2000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
-      fg[0] += CppAD::pow(vars[v_start + t] - ref_v,2);
+      fg[0] += 9000*CppAD::pow(vars[cte_start + t] - ref_cte, 2);
+      fg[0] += 9000*CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
+      fg[0] += 10*CppAD::pow(vars[v_start + t] - ref_v,2);
     }
 
     // Minimize the use of actuators
     for (int t = 0; t < N - 1; t++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 1000*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[a_start + t], 2);
       //fg[0] += 750*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2); // minimize speed while turning
     }
 
     // Minimize the value gap between sequential actuations
     for (int t = 0; t < N - 2; t++) {
-      fg[0] += 200 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t],2);
-      fg[0] += 10 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 2000 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t],2);
+      fg[0] += 1000 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
     fg[1 + x_start] = vars[x_start];
